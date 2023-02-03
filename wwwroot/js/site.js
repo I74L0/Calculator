cce22dd95a5b7223d82fe3ed7c69a7af3858035f
I74L0 <i7alog4m3r1@gmail.com>
2023-02-03 12:05:38 -0300

    Funcionality almost ready

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,25 +1,84 @@
-﻿var p = document.getElementById("p");
-var array = document.querySelectorAll(".calculator > div:not(#C)");
+﻿var num1 = "", operator = "", num2 = "";
+var p = document.getElementById("p");
+
+var array = document.querySelectorAll(".operation");
 for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+    let element = array[index];
     element.addEventListener("click", function () {
-        if (p.innerHTML.length < 8) {
-            if (p.innerHTML == "0")
-                p.innerHTML = element.firstChild.nodeValue;
-            else
-                p.innerHTML = p.innerHTML + element.firstChild.nodeValue;
+        num2 = p.innerHTML.slice(num1.length + 1);
+        if (element.innerHTML != "=") {
+            if (num1 == "") {
+                num1 = p.innerHTML;
+            } else {
+                p.innerHTML = Operation(parseFloat(num1), parseFloat(num2));
+                num1 = p.innerHTML;
+                num2 = "";
+            }
+            operator = element.innerHTML;
+        }
+        else {
+            if (num2 != "") {
+                p.innerHTML = Operation(parseFloat(num1), parseFloat(num2));
+                num1 = "", operator = "", num2 = "";
+            }
+        }
+    });
+}
+
+array = document.querySelectorAll(".calculator > div:not(#C)");
+for (let index = 0; index < array.length; index++) {
+    let element = array[index];
+    element.addEventListener("click", function () {
+        if (p.innerHTML != "0") {
+            if (num1 == "") {
+                if (p.innerHTML.length < 8) {
+                    if (element.innerHTML != "=") {
+                        p.innerHTML = p.innerHTML + element.innerHTML;
+                    }
+                }
+            } else {
+                if (p.innerHTML.length - num1.length - 1 < 8)
+                    p.innerHTML = p.innerHTML + element.innerHTML;
+            }
+        }
+        else {
+            p.innerHTML = element.innerHTML;
         }
     });
 }
 
 document.getElementById("C").addEventListener("click", function () {
-    try {
-        if (p.innerHTML != "0")
+    if (p.innerHTML.length > 1) {
+        if (p.innerHTML.length > num1.length + 1) {
             p.innerHTML = p.innerHTML.slice(0, -1);
-    } catch (error) {
-        return;
+        }
+    }
+    else {
+        p.innerHTML = "0"
     }
 });
+
 document.getElementById("AC").addEventListener("click", function () {
     p.innerHTML = "0";
+    num1 = "", operator = "", num2 = "";
 });
+
+function Operation(n1, n2) {
+    let n = 0;
+    switch (operator) {
+        case "+":
+            n = n1 + n2;
+        case "-":
+            n = n1 - n2;
+        case "x":
+            n = n1 * n2;
+        case "/":
+            n = n1 / n2;
+    }
+    n = n.toString();
+    if (n.length > 8) {
+        return 'ERR';
+    } else {
+        return n;
+    }
+}
