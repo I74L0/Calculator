1497fd29198c738b55fff420ee2eeb4d1d2335ec
I74L0 <i7alog4m3r1@gmail.com>
2023-02-03 08:28:59 -0300

    The style is ready

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Calculator.Pages;
+
+public class IndexModel : PageModel
+{
+    public static double Evaluate(string expression)
+    {
+        System.Data.DataTable table = new System.Data.DataTable();
+        table.Columns.Add("expression", string.Empty.GetType(), expression);
+        System.Data.DataRow row = table.NewRow();
+        table.Rows.Add(row);
+        return double.Parse((string)row["expression"]);
+    }
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿var p = document.getElementById("p");
+var array = document.querySelectorAll(".calculator > div:not(#C)");
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    element.addEventListener("click", function () {
+        if (p.innerHTML.length < 8) {
+            if (p.innerHTML == "0")
+                p.innerHTML = element.firstChild.nodeValue;
+            else
+                p.innerHTML = p.innerHTML + element.firstChild.nodeValue;
+        }
+    });
+}
+
+document.getElementById("C").addEventListener("click", function () {
+    try {
+        if (p.innerHTML != "0")
+            p.innerHTML = p.innerHTML.slice(0, -1);
+    } catch (error) {
+        return;
+    }
+});
+document.getElementById("AC").addEventListener("click", function () {
+    p.innerHTML = "0";
+});
